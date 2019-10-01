ec33c781c8d1a7a3e30e50cf7c2a2925ffd111ed
Arthur Lima <arthurlima@criasol.com.br>
2019-09-30 21:20:09 -0300

    [front] Mensagem de erro após redirecionamento de rota privada

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 import { Form } from "./styles";
 
 const Login = () => {
+  // TODO: Criar um custom hook pra diminuir código
+  const { state } = useLocation();
+
   return (
     <Form>
+      {state && state.message}
       <h1>Página de login</h1>
       <input type="text" placeholder="Login" />
       <input type="password" placeholder="Senha" />
