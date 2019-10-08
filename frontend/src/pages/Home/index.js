eaefe98f840d13e779cb321f1b1e79a8809bdc20
Arthur Lima <arthur-esc@hotmail.com>
2019-10-07 21:47:26 -0300

    feat: home page using logo component

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Logo from 'components/Logo';
+import { Container } from './styles';
 
 const Home = () => {
   return (
-    <div>
-      <h1>Página principal ;)</h1>
-    </div>
-  )
+    <Container>
+      <Logo />
+    </Container>
+  );
 };
 
 export default Home;
