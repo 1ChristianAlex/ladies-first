28df8b3930fec49f15360c5f30d6e4eae259fbb6
Arthur Lima <arthur-esc@hotmail.com>
2019-10-10 01:31:55 -0300

    feat: colors object and refactored imports and exports

---
+++ frontend/src/styles/Colors.js
@@ -0,0 +1,11 @@
+export default {
+  white: '#FFF',
+  black: '#000',
+  gray: '#CCC',
+  dark: '#6C6C6C',
+  lightGray: '#99A59E',
+  darkGray: '#5B5B5B',
+
+  primary: '#2CBFA5',
+  text: '#909090'
+};
---
+++ frontend/src/styles/index.js
@@ -0,0 +1,5 @@
+import * as Elements from './Elements';
+
+export { default as GlobalStyle } from './GlobalStyle';
+export { default as Colors } from './Colors';
+export { Elements };
