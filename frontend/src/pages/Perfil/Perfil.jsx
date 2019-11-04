4819ddb1b2dacd166a12380fe41c06e80061fd8b
Arthur Lima <arthur-esc@hotmail.com>
2019-11-04 18:25:38 -0300

    refac: edited perfil page to fit like the other pages

--- frontend/src/pages/Perfil/Perfil.jsx
+++ frontend/src/pages/Perfil/Perfil.jsx
@@ -1,15 +1,15 @@
 import React from "react";
+import { Layout } from 'containers';
 
 import { Container } from "./styles";
 
 const Perfil = () => {
   return (
-    <Container>
-      <h1>Perfil</h1>
-      <h2>Nome: Pessoa Exemplo</h2>
-      <h2>Idade: 99 anos</h2>
-      <h2>Sexo: Feminino</h2>
-    </Container>
+    <Layout>
+      <Container>
+        Perfil ;)
+      </Container>
+    </Layout>
   );
 };
 
