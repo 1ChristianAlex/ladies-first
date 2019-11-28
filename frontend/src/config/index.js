49f533c24dfb9b48b6aa656646f7dbee9ea37544
Arthur Lima <arthur-esc@hotmail.com>
2019-11-28 16:58:18 -0300

    chore: frontends api ruls

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,4 +1,6 @@
-export const apiUrl = 'http://localhost:3333';
+const isDev = process.env.NODE_ENV === 'development'
+
+export const apiUrl = isDev ? 'http://localhost:3333' : 'https://social-woman.herokuapp.com';
 export const appUrl = 'http://localhost:3000/';
 export const appName = 'Leaf';
 
