725fc77f1917647d664224e5562886974b244290
Arthur Lima <arthur-esc@hotmail.com>
2019-11-28 16:45:07 -0300

    chore: checking if is in production mode

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -5,7 +5,7 @@ const sequelize = new Sequelize({
   username: localEnv.DB_USER,
   password: localEnv.DB_PW,
   database: localEnv.DB_NAME,
-  host: localEnv.HOSTNAME,
+  host: localEnv.DB_HOST,
   dialect: 'mysql',
   logging: false
 });
--- backend/src/config/local.ts
+++ backend/src/config/local.ts
@@ -1,5 +1,5 @@
 import { config } from 'dotenv';
-const Mode = process.env.MODE_ENV || 'developer';
+const Mode = process.env.NODE_ENV || 'developer';
 
 if (Mode === 'developer') {
   config({ path: 'src/config/.env' }); // Here we need to pass the .env relative path folder
@@ -12,6 +12,7 @@ const envolvriment = {
   FRONTPORT: process.env.FRONT_PORT,
   BACK_END_PORT: process.env.PORT || process.env.BACK_END_PORT,
   HOSTNAME: process.env.HOSTPROJECT,
+  DB_HOST: process.env.DB_HOST,
   DB_USER: process.env.DB_USER,
   DB_PW: process.env.DB_PW,
   DB_NAME: process.env.DB_NAME,
